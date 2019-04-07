f83ac9099ae767e1c19f7d26a0b7059fb8147d1f
Richard Burk Orofeo <chardyy.orofeo@gmail.com>
2019-04-07 09:18:27 +0800

    Added the http client to the imgur struct
    Signed-off-by: Richard Burk Orofeo <chardyy.orofeo@gmail.com>

--- imgur.go
+++ imgur.go
@@ -15,12 +15,25 @@ const imgurUploadURL = "https://api.imgur.com/3/image"
 // Imgur sets the field for the required fields when uploading images to imgur.
 type Imgur struct {
 	apiKey string
+	client *http.Client
 }
 
 // New returns pointer to imgur
 func New(apiKey string) *Imgur {
+
+	var netTransport = &http.Transport{
+		Dial: (&net.Dialer{
+			Timeout: 5 * time.Second,
+		}).Dial,
+		TLSHandshakeTimeout: 5 * time.Second,
+	}
+
 	return &Imgur{
 		apiKey: apiKey,
+		client: &http.Client{
+			Timeout:   time.Second * 10,
+			Transport: netTransport,
+		},
 	}
 }
 
@@ -32,7 +45,7 @@ func (imgr *Imgur) Upload(filename string) (*http.Response, error) {
 	}
 
 	parameters := url.Values{"image": {base64.StdEncoding.EncodeToString(fileEncoded)}}
-	client := client(imgr.apiKey)
+	// client := imgr.client(imgr.apiKey)
 
 	req, err := http.NewRequest("POST", imgurUploadURL, strings.NewReader(parameters.Encode()))
 	if err != nil {
@@ -42,7 +55,7 @@ func (imgr *Imgur) Upload(filename string) (*http.Response, error) {
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 	req.Header.Set("Authorization", "Client-ID "+imgr.apiKey)
 
-	return client.Do(req)
+	return imgr.client.Do(req)
 }
 
 func client(apiKey string) *http.Client {
